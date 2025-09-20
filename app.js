e128952095190b88c58059a788db138a9bff5ea4
aadiityaasingh <adityasingh.6488@gmail.com>
2025-09-20 14:55:05 +0530

    Added .gitignore, removed node_modules and added server side error handling

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schemas.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -31,6 +32,16 @@ app.get("/", (req, res) => {
   res.send("HI i am root");
 });
 
+const validateListing = (req, res, next) => {
+  let {error} = listingSchema.validate(req.body); 
+  console.log(result);
+  if(error){
+    let erMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, erMsg)
+  } else{
+    next();
+  }
+}
 //index route
 app.get("/listings", wrapAsync( async (req, res) => {
   const allListings = await Listing.find({});
@@ -47,8 +58,7 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
   res.render("listings/show.ejs", { listing });
 }));
 
-app.post("/listings", wrapAsync(async (req, res, next) => {
-  if(!req.body.listing) { throw new ExpressError(400, "Invalid Listing Data")}
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
@@ -60,8 +70,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
   res.render("listings/edit.ejs", {listing})
 }));
 
-app.put("/listings/:id",wrapAsync(async (req,res) => {
-    if(!req.body.listing){ throw new ExpressError(400, "Invalid Listing Data")}
+app.put("/listings/:id", validateListing, wrapAsync(async (req,res) => {
     let {id} = req.params;
    await Listing.findByIdAndUpdate(id, {...req.body.listing});
    res.redirect(`/listings/${id}`)
