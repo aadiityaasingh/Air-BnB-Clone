7bb1c78820bf481385b8007b8023301cf000c309
aadiityaasingh <adityasingh.6488@gmail.com>
2025-09-26 18:52:36 +0530

    restructured

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js")
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -33,104 +32,8 @@ app.get("/", (req, res) => {
   res.send("HI i am root");
 });
 
-const validateListing = (req, res, next) => {
-  let {error} = listingSchema.validate(req.body); 
-  console.log(result);
-  if(error){
-    let erMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, erMsg)
-  } else{
-    next();
-  }
-}
-
-
-
-const validateReview = (req, res, next) => {
-  let {error} = reviewSchema.validate(req.body); 
-  console.log(result);
-  if(error){
-    let erMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, erMsg)
-  } else{
-    next();
-  }
-}
-
-
-//index route
-app.get("/listings", wrapAsync( async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-}));
-
-app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
-
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id).populate("reviews");
-  res.render("listings/show.ejs", { listing });
-}));
-
-app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-}));
-
-app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
-      let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", {listing})
-}));
-
-app.put("/listings/:id", validateListing, wrapAsync(async (req,res) => {
-    let {id} = req.params;
-   await Listing.findByIdAndUpdate(id, {...req.body.listing});
-   res.redirect(`/listings/${id}`)
-}));
-
-app.delete("/listings/:id" , wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndDelete(id)
-    res.redirect(`/listings`)
-}));
-
-app.post("/listings/:id/reviews", validateReview, wrapAsync( async (req,res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-
-  listing.reviews.push(newReview);
-  await newReview.save();
-  await listing.save();
-
-  res.redirect(`/listings/${listing._id}`);
-}))
-
-
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-  let {id, reviewId} = req.params;
-  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-
-  res.redirect(`/listings/${id}`);
-}))
-
-
-// app.get("/testListing", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "My home",
-//     description: "near the park",
-//     price: 15000,
-//     location: "chapra, bihar",
-//     country: "India",
-//   });
-//   await sampleListing.save();
-//   console.log("sammple was saved")
-//   res.send("testing success")
-// });
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
 app.all(/.*/, (req,res,next) => {
   next(new ExpressError(404, "Page Not Found"));
@@ -139,7 +42,6 @@ app.all(/.*/, (req,res,next) => {
 app.use((err, req, res, next) => {
   let {statusCode=500, message="something went wrong"} = err;
   res.status(statusCode).render("error.ejs",{message});
-  // res.status(statusCode).send(message);
 })
 
 app.listen(8080, () => {
