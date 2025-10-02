4e2aba2d25d4c87ba3b96a4b8cb646213ad9ba81
aadiityaasingh <adityasingh.6488@gmail.com>
2025-10-02 13:04:49 +0530

    adding original image in edit form

--- controllers/listings.js
+++ controllers/listings.js
@@ -44,12 +44,20 @@ module.exports.editListing = async (req, res) => {
     req.flash("error", "Cannot find that listing!");
     res.redirect("/listings");
   }
-  res.render("listings/edit.ejs", { listing });
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/ w_250")
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  if(typeof req.file !== "undefined"){//new image file ko upload krne mein help krta hai
+  let url = req.file.path;
+  let filename = req.file.filename;
+  listing.image = {url, filename}; 
+  await listing.save();
+  }
   req.flash("success", "Listing Updated!");
   res.redirect(`/listings/${id}`);
 };
