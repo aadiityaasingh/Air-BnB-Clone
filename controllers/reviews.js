2ee985b1ce043b665c980d46e137940edbda6920
Aditya Singh <147803109+aadiityaasingh@users.noreply.github.com>
2025-10-07 23:09:52 +0530

    fixes the review author error

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,15 +2,14 @@ const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 
 module.exports.createReview = async (req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-
-  listing.reviews.push(newReview);
-  await newReview.save();
+  const listing = await Listing.findById(req.params.id);
+  const newReview = new Review(req.body.review);
   newReview.author = req.user._id;
+  await newReview.save();
+  listing.reviews.push(newReview);
   await listing.save();
-  req.flash("success", "New Review Created!");
 
+  req.flash("success", "New Review Created!");
   res.redirect(`/listings/${listing._id}`);
 };
 
